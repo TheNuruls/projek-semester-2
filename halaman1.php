5b4969c0c8059872646cd7f033784f735e36f23c
TheNuruls <setianingsihnurul8@gmail.com>
2026-03-04 10:05:39 +0700

    push

--- halaman1.php
+++ halaman1.php
@@ -2,11 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <p>halloooo guys
-    </p>
+    <h1>halo word</h1>
 </body>
 </html>
